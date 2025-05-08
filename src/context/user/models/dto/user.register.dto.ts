2831d57ab254e46def14f1a4df1758f9c6c306ed
maxicattaneocvetic <cveticmaxi97@gmail.com>
2025-05-07 21:21:20 -0300

    (fix): user register documentation

--- src/context/user/models/dto/user.register.dto.ts
+++ src/context/user/models/dto/user.register.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 
-export class UserRegisterDto_response {
+export class UserRegisterDto {
     @ApiProperty({
         description: 'Name of user',
         example: "Maxi",
@@ -19,21 +19,39 @@ export class UserRegisterDto_response {
         example: "ubnet@gmail.com",
     })
     public readonly email: string;
-}
 
-
-
-
-export class UserRegisterDto extends UserRegisterDto_response {
     @ApiProperty({
         description: 'Password of user',
         example: "1234",
     })
     public readonly password: string;
 
+}
+
+
+
+
+export class UserRegisterDto_response {
+
     @ApiProperty({
-        description: 'Id of user',
-        example: "2313232564984985",
+        description: 'Name of user',
+        example: "Maxi",
     })
-    public readonly _id: string;
+    public readonly name: string;
+
+    @ApiProperty({
+        description: 'Last name of user',
+        example: "Cvetic",
+    })
+    public readonly lastName: string;
+
+    @ApiProperty({
+        description: 'Email of user',
+        example: "ubnet@gmail.com",
+    })
+    public readonly email: string;
+
+
+
+
 }
