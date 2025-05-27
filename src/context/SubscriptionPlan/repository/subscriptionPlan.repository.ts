652913df664ee3848ffd0cfbee4f15076d95cdb7
maxicattaneocvetic <cveticmaxi97@gmail.com>
2025-05-27 19:12:17 -0300

    (fix): update subscription returns object updated

--- src/context/SubscriptionPlan/repository/subscriptionPlan.repository.ts
+++ src/context/SubscriptionPlan/repository/subscriptionPlan.repository.ts
@@ -73,7 +73,7 @@ export class SubscriptionPlanRepository implements SubscriptionPlanRepositoryInt
 
     async updateSubscriptionPlanById(id: string, subscriptionPlanUpdateDto: Partial<SubscriptionPlanUpdateDto>): Promise<any> {
         try {
-            return await this.subscriptionPlanModel.updateOne({ _id: id }, subscriptionPlanUpdateDto);
+            return await this.subscriptionPlanModel.updateOne({ _id: id }, subscriptionPlanUpdateDto, { new: true });
         } catch (error: any) {
             throw error;
         }
